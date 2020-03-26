af9e1ad4d2a29b1620507ab554148510162735f3
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-03-26 16:20:36 -0300

    criada a rota de deletar  incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -18,5 +18,22 @@ module.exports = {
         })
 
         return response.json({ id })    
+    },
+
+    async delete (request,response){
+        const { id } = request.params
+        const ong_id = request.headers.authorization
+
+        const incidents = await connection("incidents")
+        .where("id", id)
+        .select("ong_id")
+        .first()
+
+        if(incidents.ong_id != ong_id){
+          return  response.status(401).json({ error: "Operation not permitted." })
+        }
+
+        await connection("incidents").where("id", id).delete()
+        return  response.status(204).send()
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,5 +10,6 @@ routes.post("/ongs", OngController.create)
 
 routes.get("/incidents",incidentController.index)
 routes.post("/incidents",incidentController.create)
+routes.delete("/incidents/:id", incidentController.delete)
 
 module.exports = routes
