52339f88d0c6a3953c1361663da1cf59bad92187
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-03-27 16:05:42 -0300

    criada a rota de login e paginacao

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const express = require("express")
 const OngController = require("./controllers/OngsController")
 const IncidentController = require("./controllers/incidentController")
 const ProfileController = require("./controllers/ProfileController")
+const SessionController = require("./controllers/SessionController")
 
 const routes = express.Router()
+
+routes.post("/sessions", SessionController.create)
 
 routes.get("/ongs",OngController.index )
 routes.post("/ongs", OngController.create)
