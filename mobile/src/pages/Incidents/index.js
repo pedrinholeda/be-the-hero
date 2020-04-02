604ec558144d507be4393926497543901bbfff5a
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-04-02 17:48:07 -0300

    Feita tela de incidents

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {Feather} from "@expo/vector-icons"
+import {View, FlatList,TouchableOpacity, Image, Text} from "react-native"
+
+import logoImg from "../../assets/logo.png"
+
+import styles from "./styles"
+
+export default function Incidents(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}> 0 casos</Text>.
+                </Text>
+            </View>
+            <Text style={styles.title}>Bem Vindo!</Text>
+            <Text style={styles.description}> Escolha um dos casos abaixo e salve o dia. </Text>
+
+            <FlatList
+                data={[1,2,3,4,5]}
+                style={styles.incidentList}
+                renderItem={() => (
+                    <View style={styles.incidentList}>
+                <View style={styles.incident}>
+                    <Text style={styles.incidentProperty}>ONG:</Text>
+                    <Text style={styles.incidentValue}>APAD</Text>
+
+                    <Text style={styles.incidentProperty}>CASO:</Text>
+                    <Text style={styles.incidentValue}>Tratamento de doguinho</Text>
+
+                    <Text style={styles.incidentProperty}>VALOR:</Text>
+                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                    <TouchableOpacity 
+                        style={styles.detailsButton} 
+                        onPress={ () => {} }
+                    >
+                        <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                        <Feather name="arrow-right" size={17} color="#E02041"/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+                )}
+            />
+        </View>
+    )
+}
