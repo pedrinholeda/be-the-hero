7bf4b9314d0619d4c2bedf8e926bdbb655642b1f
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-07-06 21:15:03 -0300

    Finalizado o app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,7 +15,7 @@ routes.post("/ongs", celebrate({
     [Segments.BODY]: Joi.object().keys({
         name : Joi.string().required,
         email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10).max(13),
+        whatsapp: Joi.string().required().min(10).max(13),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2),
     })
@@ -28,14 +28,14 @@ routes.get("/profile", celebrate({
 }), ProfileController.index)
 
 routes.get("/incidents",celebrate({
-    [Segments.QUERY]:Joi.object.keys({
+    [Segments.QUERY]:Joi.object().keys({
         page: Joi.number()
     })
 }),IncidentController.index)
 
 routes.post("/incidents",IncidentController.create)
 routes.delete("/incidents/:id", celebrate({
-  [Segments.PARAMS]: Joi.object.keys({
+  [Segments.PARAMS]: Joi.object().keys({
       id: Joi.number().required()
   })  
 }),IncidentController.delete)
